05cfa797006be6e567080ec06a8a69c86642662a
Willian Florentino <williandimi11@gmail.com>
2024-02-13 09:51:13 -0300

    att

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 import express from 'express';
+import process from 'process';
+import path from 'path';
 
 const host='0.0.0.0';
 const porta = 3000; 
 
 const app = express();
 
-app.get('/', (pega, devolve) => {
-    devolve.write('<h1> Seja bem-vindo ao site!</h1>')
-    devolve.end();
-} );
-
-app.get('/index.html', (pega, devolve) => {
-    devolve.write('<h1> Esse é o index.html</h1>')
-    devolve.end();
-} );
+app.use(express.static(path.join(process.cwd(), 'publico')));
 
 app.listen(porta, host, ()=>{
     console.log(`Servidor escutando em http://${host}:${porta}`);
